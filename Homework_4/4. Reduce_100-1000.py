452b73fca65b7b15c15c3b020db9e5c8b41e535c
Karadesh <akamalavr@gmail.com>
2020-05-23 21:13:51 +0300

    Revert "Homework_5 dir added"
    This reverts commit edf624329e780fdb1ebef63f788ac1b8e1c4dc10.

--- Homework_4/4. Reduce_100-1000.py
+++
@@ -1,8 +0,0 @@
-from functools import reduce
-
-elements_of_thousand = [el for el in range(100,1000) if el %2 == 0]
-even_numbers = reduce(lambda x , y : x * y, elements_of_thousand)
-print(even_numbers)
-#print(elements_of_thousand)
-
-#print(division_of_thousands)
