2fa563cb2143b5f62ce7e395dc7fadce67597347
Karadesh <akamalavr@gmail.com>
2020-05-25 14:01:07 +0300

    Homework_5 from 1 to 5 added

--- Homework_5/1Text001.py
+++ Homework_5/1Text001.py
@@ -4,6 +4,7 @@ text_creator = open('Text1.txt', 'a')
 writer = None
 for i in count(1):
     if writer == '':
+        text_creator.write('\n')
         break
     writer = (input('Введите текст. Нажмите на Enter, чтобы перестать: '))
     text_creator.write(writer)
